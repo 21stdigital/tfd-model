f186c353d2138e97b7af89a21cb17f00b28f9904
Steffen Rüther <steffen@21st.digital>
2019-05-20 15:50:49 +0200

    - added first image SizeGroup class Square for testing

---
+++ src/image/sizes/Square.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TFD\Image;
+
+class Square extends SizeGroup
+{
+    public $sources =  [
+        [1200, 620, 620],
+        [992, 560, 560],
+        [768, 420, 420],
+        [576, 360, 360],
+        [0, 375, 256],
+    ];
+}
