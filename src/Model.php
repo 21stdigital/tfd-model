889523663d687268f0dc052fa5abb226b2a7b632
Steffen Rüther <steffen@21st.digital>
2019-09-17 16:26:08 +0200

    - added magic isset function to model

--- src/Model.php
+++ src/Model.php
@@ -4,4 +4,13 @@ namespace TFD;
 
 class Model extends \WP_Model
 {
+    /**
+    * @return void
+    */
+    public function __isset($attribute)
+    {
+        error_log("ISSET {$attribute}");
+        $value = $this->$attribute;
+        return !(is_null($value) || $value = '');
+    }
 }
