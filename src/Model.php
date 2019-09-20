496fb1ea56a6532beadb43d1a6c2baf4e19a6561
Steffen Rüther <steffen@21st.digital>
2019-09-20 12:58:33 +0200

    - removed error logs

--- src/Model.php
+++ src/Model.php
@@ -9,7 +9,6 @@ class Model extends \WP_Model
     */
     public function __isset($attribute)
     {
-        error_log("ISSET {$attribute}");
         $value = $this->$attribute;
         return !(is_null($value) || $value = '');
     }
