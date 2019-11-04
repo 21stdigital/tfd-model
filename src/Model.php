6d0961fbe60579545658537e82ab58e2a536da65
Steffen Rüther <steffen@21st.digital>
2019-11-04 18:08:26 +0100

    - added convinient field function

--- src/Model.php
+++ src/Model.php
@@ -12,4 +12,16 @@ class Model extends \WP_Model
         $value = $this->$attribute;
         return !(is_null($value) || $value = '');
     }
+
+    public function field($key, $default = null)
+    {
+        $attribute = ($this->prefix.$key);
+        if (function_exists('field')) {
+            return field($attribute, $this->ID)->default($default)->get();
+        } elseif (function_exists('get_field')) {
+            return get_field($attribute, $this->ID) ?: $default;
+        } else {
+            return get_post_meta($this->ID, $attribute, true) ?: $default;
+        }
+    }
 }
